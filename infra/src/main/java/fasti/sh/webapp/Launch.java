efbbdcd4d8a19903cde77e22d941bf892b02e9a3
stxkxs <brandon.stokes@hey.com>
2025-12-03 01:59:10 -0800

    refactor tags and simplify synthesizer configuration
    - replace part-of composite tag with organization, name, alias fields
    - update deployment:name and deployment:alias to use team namespace
    - remove custom synthesizer logic from Launch.java
    - update ses context keys from hosted to deployment prefix
    - delete synthesizer.mustache

--- infra/src/main/java/fasti/sh/webapp/Launch.java
+++ infra/src/main/java/fasti/sh/webapp/Launch.java
@@ -6,19 +6,15 @@ import static fasti.sh.execute.serialization.Format.name;
 import com.fasterxml.jackson.core.type.TypeReference;
 import fasti.sh.execute.serialization.Mapper;
 import fasti.sh.execute.serialization.Template;
-import fasti.sh.model.aws.cdk.Synthesizer;
 import fasti.sh.model.main.Common;
 import fasti.sh.model.main.Release;
-
 import fasti.sh.webapp.stack.WebappReleaseConf;
 import fasti.sh.webapp.stack.WebappStack;
 import java.util.Map;
 import lombok.SneakyThrows;
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.DefaultStackSynthesizer;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.StackSynthesizer;
 
 public class Launch {
 
@@ -47,7 +43,6 @@ public class Launch {
                 "%s %s webapp",
                 conf.release().common().name(),
                 conf.release().common().alias())))
-        .synthesizer(synthesizer(app))
         .tags(Common.Maps.from(conf.platform().tags(), conf.release().common().tags()))
         .build());
 
@@ -60,79 +55,11 @@ public class Launch {
       .parse(
         app,
         "conf.mustache",
-        Map
-          .ofEntries(
-            Map.entry("hosted:ses:hosted:zone", app.getNode().getContext("hosted:ses:hosted:zone")),
-            Map.entry("hosted:ses:email", app.getNode().getContext("hosted:ses:email"))));
+        Map.ofEntries(
+            Map.entry("deployment:ses:hosted:zone", app.getNode().getContext("deployment:ses:hosted:zone")),
+            Map.entry("deployment:ses:email", app.getNode().getContext("deployment:ses:email"))));
 
     var type = new TypeReference<Release<WebappReleaseConf>>() {};
     return Mapper.get().readValue(parsed, type);
-  }
-
-  @SneakyThrows
-  private static StackSynthesizer synthesizer(App app) {
-    var customSynthesizer = Boolean.parseBoolean(app.getNode().getContext("synthesizer:custom").toString());
-
-    if (customSynthesizer) {
-      var key = app.getNode().getContext("hosted:api:key");
-      var synthesizerName = app.getNode().getContext("hosted:synthesizer:name");
-      var synthesizerAlias = app.getNode().getContext("hosted:synthesizer:alias");
-
-      var parsed = Template
-        .parse(
-          app,
-          "synthesizer.mustache",
-          Map
-            .ofEntries(
-              Map.entry("synthesizer:role:exec", app.getNode().getContext("hosted:cdk:role:exec").toString()),
-              Map.entry("synthesizer:role:deploy", app.getNode().getContext("hosted:cdk:role:deploy").toString()),
-              Map.entry("synthesizer:role:lookup", app.getNode().getContext("hosted:cdk:role:lookup").toString()),
-              Map.entry("synthesizer:role:assets", app.getNode().getContext("hosted:cdk:role:assets").toString()),
-              Map.entry("synthesizer:role:images", app.getNode().getContext("hosted:cdk:role:images").toString()),
-              Map.entry("synthesizer:ssm", app.getNode().getContext("hosted:cdk:keys:ssm").toString()),
-              Map.entry("synthesizer:storage:assets", app.getNode().getContext("hosted:cdk:storage:assets").toString()),
-              Map.entry("synthesizer:storage:images", app.getNode().getContext("hosted:cdk:storage:images").toString()),
-              Map.entry("synthesizer:qualifier", String.format("%s-%s", synthesizerName, synthesizerAlias)),
-              Map.entry("synthesizer:assets:prefix", String.format("%s-%s", synthesizerName, synthesizerAlias)),
-              Map.entry("synthesizer:images:tag", String.format("%s-%s", synthesizerName, synthesizerAlias)),
-              Map.entry("synthesizer:externalid", key)));
-
-      var synthesizer = Mapper.get().readValue(parsed, Synthesizer.class);
-      return DefaultStackSynthesizer.Builder
-        .create()
-        .qualifier(synthesizer.qualifier())
-        .cloudFormationExecutionRole(synthesizer.cloudFormationExecutionRole())
-        .deployRoleArn(synthesizer.deployRoleArn())
-        .lookupRoleArn(synthesizer.lookupRoleArn())
-        .fileAssetPublishingRoleArn(synthesizer.fileAssetPublishingRoleArn())
-        .imageAssetPublishingRoleArn(synthesizer.imageAssetPublishingRoleArn())
-        .bootstrapStackVersionSsmParameter(synthesizer.bootstrapStackVersionSsmParameter())
-        .fileAssetsBucketName(arnToBucketName(synthesizer.fileAssetsBucketName()))
-        .imageAssetsRepositoryName(synthesizer.imageAssetsRepositoryName())
-        .generateBootstrapVersionRule(synthesizer.generateBootstrapVersionRule())
-        .useLookupRoleForStackOperations(synthesizer.useLookupRoleForStackOperations())
-        .bucketPrefix(synthesizer.bucketPrefix())
-        .dockerTagPrefix(synthesizer.dockerTagPrefix())
-        .lookupRoleExternalId(synthesizer.lookupRoleExternalId())
-        .fileAssetPublishingExternalId(synthesizer.fileAssetPublishingExternalId())
-        .imageAssetPublishingExternalId(synthesizer.imageAssetPublishingExternalId())
-        .deployRoleExternalId(synthesizer.deployRoleExternalId())
-        .build();
-    } else {
-      return DefaultStackSynthesizer.Builder.create().build();
-    }
-  }
-
-  public static String arnToBucketName(String arn) {
-    if (arn != null && arn.startsWith("arn:aws:s3::")) {
-      var parts = arn.split(":");
-      var resourcePart = parts[parts.length - 1];
-      if (resourcePart.startsWith("/")) {
-        resourcePart = resourcePart.substring(1);
-      }
-      var resourceParts = resourcePart.split("/");
-      return resourceParts[0];
-    }
-    return null;
   }
 }
